16b7cd9c31a82de5183e8be61dd671d26f63977d
Abinash <abinash@Abinashs-MacBook-Pro.local>
2019-12-18 15:47:32 -0500

    added componenet scan

---
+++ src/main/java/com/rab3/dtos/TeamDto.java
@@ -0,0 +1,47 @@
+package com.rab3.dtos;
+
+public class TeamDto {
+
+	private String name;
+	private String country;
+	private String mngr;
+	private String logo;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getMngr() {
+		return mngr;
+	}
+
+	public void setMngr(String mngr) {
+		this.mngr = mngr;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	@Override
+	public String toString() {
+		return "TeamDto [name=" + name + ", country=" + country + ", mngr=" + mngr + ", logo=" + logo + "]";
+	}
+
+}
